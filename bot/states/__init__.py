91b11b456dc9bae7ac51abe198db2b95c3b432d5
gabrielagrela <gabrielagrela99@gmail.com>
2025-08-30 20:13:33 +0100

    Remove 'farm_ore_and_scout' state implementation from the bot's state management. This includes the deletion of the AlternatingState class and its associated build function, streamlining the codebase.

--- bot/states/__init__.py
+++ bot/states/__init__.py
@@ -1,7 +1,6 @@
 from .scouts import build_scouts_state
 from .farm_wood import build_farm_wood_state
 from .farm_ore import build_farm_ore_state
-from .farm_ore_and_scout import build_farm_ore_and_scout_state
 from .alternate import build_alternating_state, build_round_robin_state
 from .farm_gold import build_farm_gold_state
 from .alliance_help import build_alliance_help_state
@@ -10,7 +9,6 @@ __all__ = [
     "build_scouts_state",
     "build_farm_wood_state",
     "build_farm_ore_state",
-    "build_farm_ore_and_scout_state",
     "build_alternating_state",
     "build_round_robin_state",
     "build_farm_gold_state",
